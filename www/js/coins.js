984933953d5397a84bd8c1368a56ca92b77a4bf1
fsw <frank@wawrzak.com>
2018-01-13 00:56:49 +0100

    dirty app draft cd. support for ERC20 tokens

---
+++ www/js/coins.js
@@ -0,0 +1,77 @@
+
+var BtcHandler = {
+    name: "bitcoin",
+    code: "BTC",
+    longname: "Bitcoin"
+}
+
+var XrpHandler = {
+    name: "ripple",
+    code: "XRP",
+    longname: "ripple"
+}
+
+var BchHandler = {
+    name: "bitcoin-cash",
+    code: "BCH",
+    longname: "Bitcoin Cash"
+}
+
+var LtcHandler = {
+    name: "litecoin",
+    code: "LTC",
+    longname: "Litecoin"
+}
+
+
+web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/O7OJXaza9lovtjycsQWS"));
+
+var EthHandler = {
+    name: "ethereum",
+    code: "ETH",
+    longname: "Ethereum",
+    getBalance: function(addr, callback){
+      web3.eth.getBalance(addr).then(function(val){
+        callback(val == 0 ? 0.0 : parseFloat(web3.utils.fromWei(val, 'ether')));
+      });
+    },
+
+    getLocalAddr: function(){
+      return web3.eth.accounts.privateKeyToAccount(getEtherPrivateKey()).address;
+      //console.log(web3.eth.accounts.create());
+    }
+
+}
+
+function getEtherPrivateKey() {
+  var storage = window.localStorage;
+  var value = storage.getItem('ethPK');
+  if (!value) {
+    value = prompt("Please enter PK", "");
+    storage.setItem('ethPK', value);
+  }
+  return value;
+}
+
+var PayHandler = {
+    //https://etherscan.io/token/TenXPay?a=0xab22ee7d5f3d2bb9416ea2b71b822f3097419d0a
+    name: "tenx",
+    code: "PAY",
+    longname: "TenX",
+    ethContractAddr: '0xB97048628DB6B661D4C2aA833e95Dbe1A905B280',
+    ethAbi: [{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"startTrading","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tradingStarted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}],
+    getBalance: function(addr, callback){
+      var contract = new web3.eth.Contract(this.ethAbi, this.ethContractAddr);
+      contract.methods.balanceOf(addr).call().then(function(val){
+        callback(val == 0 ? 0.0 : parseFloat(web3.utils.fromWei(val, 'ether')));
+      });
+      /*, function(err, ok){
+        console.log("TENX");
+        console.log(ok);
+      }));*/
+    },
+    getLocalAddr: function(){
+      return web3.eth.accounts.privateKeyToAccount(getEtherPrivateKey()).address;
+      //console.log(web3.eth.accounts.create());
+    }
+}
--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,4 @@
 
-var BtcHandler = {
-    code: "BTC"
-}
-
-var EthHandler = {
-    code: "ETH",
-    getBalance: function(addr){
-        web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/O7OJXaza9lovtjycsQWS"));
-        return parseFloat(web3.fromWei(web3.eth.getBalance(addr).toNumber(), 'ether'));
-    }
-}
 
 function formatMoney(value, unit){
   return value.toFixed(2).replace(/./g, function(c, i, a) {
@@ -17,26 +6,58 @@ function formatMoney(value, unit){
   }) + '&nbsp;' + unit;
 };
 
+
+var activeWallet = null;
+
+function createButton(icon, callback) {
+  var button = document.createElement("img");
+  button.setAttribute('src', 'icons/if_' + icon + '.png');
+  button.onclick = callback;
+  button.classList.add('button');
+  return button;
+}
+
 function Wallet(handler, offlineWallets) {
 
+
   this.row = document.createElement("tr");
+  var that = this;
+  this.row.onclick = function() {
+    if (activeWallet) {
+      activeWallet.row.classList.remove('active');
+    }
+    that.row.classList.add('active');
+    activeWallet = that;
+  }
 
   var unitCell = document.createElement("td");
-  unitCell.appendChild(document.createTextNode(handler.code));
+  unitCell.innerHTML = '<img src="coins/' + handler.name + '.png" alt="' + handler.code + '"/>';
 
   var onlineCell = document.createElement("td");
+  onlineCell.classList.add('online');
   this.onlineAmount = document.createElement("div");
   this.onlineValue = document.createElement("div");
   onlineCell.appendChild(this.onlineValue).classList.add('value');
   onlineCell.appendChild(this.onlineAmount).classList.add('amount');
 
+  if ('getLocalAddr' in handler) {
+
+
+    onlineCell.appendChild(createButton('arrow_up', function(){}));
+    onlineCell.appendChild(createButton('arrow_down', function(){}));
+
+  } else {
+    onlineCell.classList.add('disabled');
+  }
 
   var offlineCell = document.createElement("td");
-
+  offlineCell.classList.add('offline');
   this.offlineAmount = document.createElement("div");
   this.offlineValue = document.createElement("div");
   offlineCell.appendChild(this.offlineValue).classList.add('value');
   offlineCell.appendChild(this.offlineAmount).classList.add('amount');
+
+  offlineCell.appendChild(createButton('pencil', function(){}));
 
   this.row.appendChild(unitCell);
   this.row.appendChild(onlineCell);
@@ -55,7 +76,9 @@ function Wallet(handler, offlineWallets) {
 
   this.updateOnlineValue = function() {
     var value = 0;
+    console.log(handler.code);
     if (handler.code in app.prices ) {
+      console.log(this.totalOnline);
       value = this.totalOnline * app.prices[handler.code];
     }
     this.onlineValue.innerHTML = formatMoney(value, 'PLN');
@@ -65,21 +88,34 @@ function Wallet(handler, offlineWallets) {
 
   this.refreshOffline = function() {
       this.totalOffline = 0;
-      for (idx in this.offlineWallets) {
-        this.totalOffline += handler.getBalance(this.offlineWallets[idx]);
-      }
       this.offlineAmount.innerHTML = formatMoney(this.totalOffline, handler.code);
       this.updateOfflineValue();
-      return this.totalOffline;
+
+      for (var idx in this.offlineWallets) {
+        handler.getBalance(this.offlineWallets[idx], function(val){
+          console.log(val);
+          that.totalOffline += val;
+          that.offlineAmount.innerHTML = formatMoney(that.totalOffline, handler.code);
+          that.updateOfflineValue();
+        });
+      }
   }
 
   this.refreshOffline();
 
   this.refreshOnline = function() {
-    this.totalOnline = 0.05 + (Math.random() / 10);
+    this.totalOnline = 0;
     this.onlineAmount.innerHTML = formatMoney(this.totalOnline, handler.code);
     this.updateOnlineValue();
-    return this.totalOnline;
+
+    if ('getLocalAddr' in handler) {
+      handler.getBalance(handler.getLocalAddr(), function(val){
+        that.totalOnline = val;
+        that.onlineAmount.innerHTML = formatMoney(that.totalOnline, handler.code);
+        that.updateOnlineValue();
+      });
+    }
+
   }
 
   this.refreshOnline();
@@ -97,16 +133,17 @@ var app = {
     updateMarketCap: function() {
       document.getElementById('refresh').innerHTML = '...';
       var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://api.coinmarketcap.com/v1/ticker/?convert=PLN&limit=10');
+      xhr.open('GET', 'https://api.coinmarketcap.com/v1/ticker/?convert=PLN&limit=200');
       xhr.onload = function() {
           if (xhr.status === 200) {
               var list = JSON.parse(this.responseText);
-              for (i in list) {
+              for (var i in list) {
                 app.prices[list[i]['symbol']]=parseFloat(list[i]['price_pln']);
               }
               var totalOnline = 0;
               var totalOffline = 0;
-              for (i in app.wallets) {
+              for (var i in app.wallets) {
+                console.log('TEST' + i);
                 totalOnline += app.wallets[i].updateOnlineValue();
                 totalOffline += app.wallets[i].updateOfflineValue();
               }
@@ -128,7 +165,13 @@ var app = {
         var eth = new Wallet(EthHandler, ['0x87Fdb041d00597067Ed5F52dbA73d140130Ae787']);
         document.getElementById('walletsList').appendChild(eth.row);
 
-        this.wallets = [btc, eth];
+        var pay = new Wallet(PayHandler, []);
+        document.getElementById('walletsList').appendChild(pay.row);
+
+        var ltc = new Wallet(LtcHandler, []);
+        document.getElementById('walletsList').appendChild(ltc.row);
+
+        this.wallets = [btc, eth, pay, ltc];
         this.updateMarketCap();
     },
 
