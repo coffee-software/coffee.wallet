bb1d24181d976be964e2a812487b6b7626757170
fsw <frank@wawrzak.com>
2018-02-20 23:54:31 +0100

    not required

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -21,7 +21,7 @@ git tag $nextVersion
 
 #build zip
 rm -rf build.zip
-zip -qr build.zip hooks node_modules platforms plugins res www LICENSE.txt package.json config.xml ACKNOWLEDGEMENTS.md
+zip -qr build.zip hooks node_modules plugins res www LICENSE.txt package.json config.xml ACKNOWLEDGEMENTS.md
 
 #deploy website
 rsync -avzph landing_page/ coffee:wallet.coffee/
