3d8ebedfd490a076dc2974d41a844be2f7d1d480
fsw <frank@wawrzak.com>
2018-06-14 14:18:32 +0200

    added changelog

--- www/js/changelog.js
+++ www/js/changelog.js
@@ -1,12 +1,23 @@
-window.version = '0.1.8';
+window.version = '0.1.9';
 window.changelog = [
+  {
+    version: '0.1.9',
+    date: '2018-06-14',
+    changes: [
+      'added: tools tab',
+      'added: send and receive coins via message feature',
+      'added: scan/paste any code tool that detects coin and open given send form (or to receive coins from message)',
+      'fixed: show warning when scanning different coin address when on send form.',
+      'fixed: detecting proper coin on BIP72 address (so it can be used in "scan any" tool)'
+    ]
+  },
   {
     version: '0.1.8',
     date: '2018-05-31',
     changes: [
-      'DOGE coin wallet support',
-      'BIP39 support; private keys are generated from 12-word mnemonic passphrase',
-      'BIP70 support (bitpay payments)'
+      'added: DOGE coin wallet support',
+      'added: BIP39 support; private keys are generated from 12-word mnemonic passphrase',
+      'added: BIP70 support (bitpay payments)'
     ]
   },
   {
