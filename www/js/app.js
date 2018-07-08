402d04f9069f8ddd93f6ac7894ee1712f83ca9a8
fsw <frank@wawrzak.com>
2018-07-08 21:02:31 +0200

    log date display fix

--- www/js/app.js
+++ www/js/app.js
@@ -157,7 +157,7 @@ var app = {
           var li = document.createElement('li');
           li.classList.add('msg');
           li.classList.add(logs[i].severity);
-          li.innerHTML = '<div class="ts">' + (new Date(logs[i].ts * 1000)).toUTCString()  + '</div><div>' + logs[i].message + '</div>';
+          li.innerHTML = '<div class="ts">' + (new Date(logs[i].ts)).toUTCString()  + '</div><div>' + logs[i].message + '</div>';
           //logs[i].coin
           document.getElementById('history').appendChild(li);
         }
