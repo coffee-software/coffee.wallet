a97a342c4358ff9d5a1f876b183f4f8f5562f4ca
fsw <frank@wawrzak.com>
2018-11-17 10:52:47 +0100

    prapare pseudo-inheritance

--- www/js/core.js
+++ www/js/core.js
@@ -1,5 +1,15 @@
 'use strict'
 
+function ExtendObject(proto, fields){
+  var child = Object.create(proto);
+  for(var f in fields) {
+    if (fields.hasOwnProperty(f)) {
+      child[f] = fields[f];
+    }
+  }
+  return child;
+}
+
 if (!String.prototype.startsWith)
   String.prototype.startsWith = function (str) {
     return !!(str && this) && !this.lastIndexOf(str, 0)
@@ -10,14 +20,3 @@ if (!String.prototype.endsWith)
     var offset = str && this ? this.length - str.length : -1
     return offset >= 0 && this.lastIndexOf(str, offset) === offset
   }
-
-
-function ExtendObject(proto, fields){
-  var child = Object.create(proto);
-  for(var f in fields) {
-    if (fields.hasOwnProperty(f)) {
-      child[f] = fields[f];
-    }
-  }
-  return child;
-}
