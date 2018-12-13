1697b49feacabf62455bc70008635951bee3d52b
fsw <frank@wawrzak.com>
2018-12-13 13:34:52 +0100

    #30 desktop qr code scanner

--- electron/www/cordova.js
+++ electron/www/cordova.js
@@ -4,6 +4,7 @@
 
 var electron = require('electron');
 var eStorage = require('electron-json-storage');
+var Instascan = require('instascan');
 
 
 NativeStorage = {
@@ -42,9 +43,55 @@ navigator.splashscreen = {
 cordova = {
   plugins: {
     barcodeScanner: {
-      scan: function(success, error, options){
+      cameraId: 0,
+      cameras: [],
+      scanner: null,
+      initialized: false,
+      scanSuccess: null,
+      scanError: null,
 
-        alert('available in mobile version only');
+      nextCamera: function(){
+        this.cameraId ++;
+        if (this.cameraId >= this.cameras.length) {
+          this.cameraId = 0;
+        }
+        this.scanner.start(this.cameras[this.cameraId]);
+        document.getElementById('mobileQRCamera').innerHTML = this.cameras[this.cameraId].name;
+      },
+
+      closeScanner: function(){
+        document.getElementById('mobileQRScanner').classList.remove('show');
+        this.scanner.stop();
+      },
+
+      scan: function(success, error, options){
+        var that = this;
+        document.getElementById('mobileQRScanner').classList.add('show');
+        that.scanSuccess = success;
+        that.scanError = error;
+        if (!this.initialized) {
+          that.scanner = new Instascan.Scanner({ video: document.getElementById('mobileQRPreview') });
+          document.getElementById('mobileQRCancel').onclick = that.closeScanner.bind(that);
+          document.getElementById('mobileQRCamera').onclick = that.nextCamera.bind(that);;
+          that.initialized = true;
+          that.scanner.addListener('scan', function (content) {
+            that.closeScanner();
+            that.scanSuccess({text:content});
+          });
+        }
+        Instascan.Camera.getCameras().then(function (cameras) {
+          if (cameras.length > 0) {
+            that.cameras = cameras;
+            that.cameraId = -1;
+            that.nextCamera();
+          } else {
+            that.closeScanner();
+            that.scanError('No cameras found.');
+          }
+        }).catch(function (e) {
+          that.closeScanner();
+          that.scanError(e);
+        });
       }
     },
     clipboard: {
