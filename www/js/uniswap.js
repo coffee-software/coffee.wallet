6687e818fb8b307dd769a7be5cf4489bc1165fc3
fsw <frank@wawrzak.com>
2019-08-02 19:46:18 +0200

    #42 exchange provider api

---
+++ www/js/uniswap.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var uniswap = {
+
+  getCurrencies : function(callback) {
+  },
+
+  getMinAmount : function(from, to, callback) {
+  },
+
+  getExchangeAmount : function(from, to, amount, callback) {
+  },
+
+  createTransaction : function(from, to, amount, returnTo, callback) {
+  },
+
+}
