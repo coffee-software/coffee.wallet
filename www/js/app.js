2a26c8961fbbd01ed68d9225f4432ae1038ffc0d
fsw <frank@wawrzak.com>
2019-08-07 12:13:20 +0200

    popup bugfix

--- www/js/app.js
+++ www/js/app.js
@@ -884,6 +884,7 @@ var app = {
                 coin.code in app.data.wallets && app.data.wallets[coin.code].enabled,
                 function(coin){
                   app.closePopup();
+                  document.getElementById('addCoinFilter').blur();
                   setTimeout(function(){
                     app.addOrActivateCoin(coin.code, function(){
                       coin._button.classList.add('active');
