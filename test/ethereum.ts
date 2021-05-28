a4639f4cd48520fb2736e633baed9aaebf8ca9e5
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2021-05-28 13:24:18 +0200

    key generation bruteforce test

--- test/ethereum.ts
+++ test/ethereum.ts
@@ -6,9 +6,41 @@ import {CacheMock, CacheWrapperMock, LogMock, StorageMock} from "./_mocks";
 import {Engine} from "../src/Engine";
 
 describe('Ethereum Test', function() {
+    /* describe('randomizer', function () {
+        let engine = new Engine(
+            new StorageMock(),
+            new LogMock(),
+            new CacheMock()
+        );
+        let maxMatching = 0;
+        for (let k = 0; k< 1000; k++) {
+            console.log(k);
+            for (let i = 0; i < 500; i++) {
+                let mnemo = Keychain.newMnemonic();
+                let keychain = new Keychain(mnemo);
+                let addr = new HandlerEth(engine).getReceiveAddr(keychain);
+                let matching = 0;
+                for (let j = 2; j < addr.length; j++) {
+                    //console.log(addr.charAt(j), matching.toString());
+                    if (addr.charAt(j) != (matching+1).toString()) {
+                        break;
+                    }
+                    matching++;
+                    if (matching > maxMatching) {
+                        maxMatching = matching;
+                        console.log('new best');
+                        console.log(mnemo);
+                        console.log(addr);
+                    } else if (matching == maxMatching) {
+                        console.log('remis');
+                        console.log(mnemo);
+                        console.log(addr);
+                    }
+                }
+            }
+        }
+    }) */
     describe('mnemonics', function () {
-        let keychain1 = new Keychain(mnemonic1);
-        let keychain2 = new Keychain(mnemonic2);
         let engine = new Engine(
             new StorageMock(),
             new LogMock(),
